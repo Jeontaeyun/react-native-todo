9571b9d3ddfaaab6b29fae64316b0f6f117bb0dd
jeontaeyun <jow1813@naver.com>
2019-10-22 18:47:42 +0900

    chore : add bundle scripts command

---
+++ README.md
@@ -0,0 +1,2 @@
+# React Native To do list 예제
+
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * React-Native 진입 파일
  */
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
