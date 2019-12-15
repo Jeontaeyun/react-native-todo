9214f3e6c8bea2f114a5f04e349b2fa371bc631a
jeontaeyun <jow1813@naver.com>
2019-12-15 23:48:40 +0900

    feature : setting react native

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
-import Heading from './components/Heading';
-import InputText from './components/TextInput';
-import Button from './components/Button';
-import TodoList from './container/TodoList';
-import TabBar from './container/TabBar';
+import {
+	StyleSheet,
+	View,
+	Animated,
+	Button
+} from "react-native";
 /**
  * ScrollView는 플랫폼을 감싸는 것으로 스크롤이 가능한 View 컴포넌트 입니다.
  * keyboardShouldPersistTaps속성은 키보드각 열리 있으면 닫아서 UI가 onPress이벤트를 모두 처리하게 합니다.
@@ -16,84 +16,42 @@ import TabBar from './container/TabBar';
 let todoIndex = 0;
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			inputValue: '',
-			todos: [],
-			type: 'All'
-		};
+	//marginTop이라는 클래스 속성을 만들고 애니메이션 효과를 만들고, 시작 값을 전달(20)
+	marginTop = new Animated.Value(20);
+	
+	animate = () => {
+			Animated.timing(
+				 this.marginTop,
+				{
+					toValue: 200,
+					duration: 500
+				}
+			).start();
 	}
-	inputChange = (inputValue) => {
-		this.setState({ inputValue });
-	};
-	submitTodo = () => {
-		if (this.state.inputValue.match(/^\s*$/)) {
-			return null;
-		}
-		const todo = {
-			title: this.state.inputValue,
-			todoIndex,
-			complete: false
-		};
-		todoIndex++;
-		const todos = [ ...this.state.todos, todo ];
-		this.setState({ todos, inputValue: '' }, () => {
-			/**
-			 * setState 뒤에 상태 설정이ㅣ 완료되면 콜백 함수를 전달하는 옵션이 있다.
-			 * 이 기능을 기억해 둡시다.
-			 */
-			console.log('State: ', this.state);
-		});
-	};
-	deleteTodo = (index) => {
-		let { todos } = this.state;
-		todos = todos.filter((todo) => todo.todoIndex !== index);
-		this.setState({ todos });
-	};
-	toggleComplete = (index) => {
-		let { todos } = this.state;
-		todos.forEach((todo) => {
-			if (todo.todoIndex === index) {
-				todo.complete = !todo.complete;
-			}
-		});
-		this.setState({ todos });
-	};
-	setType = (type) => {
-		this.setState({ type });
-	};
-
-	render() {
-		const { inputValue, todos, type } = this.state;
-		return (
-			<View style={styles.container}>
-				<ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
-					<Heading />
-					<InputText inputValue={inputValue} inputChange={(text) => this.inputChange(text)} />
-					<TodoList
-						todos={todos}
-						type={type}
-						deleteTodo={this.deleteTodo}
-						toggleComplete={this.toggleComplete}
-					/>
-					<Button submitTodo={this.submitTodo} />
-				</ScrollView>
-				<TabBar type={type} setType={this.setType} />
+	render(){
+		return(<View style={styles.container}>
+				<Button
+						title = "Animate Box"
+						onPress={this.animate}
+				/>
+				<Animated.View style={[styles.box, {marginTop : this.marginTop }]} />
 			</View>
-		);
+		)
 	}
 }
 
-const styles = StyleSheet.create({
+const styles= StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#f5f5f5'
+		padding: 10,
+		paddingTop: 50
 	},
-	content: {
-		flex: 1,
-		paddingTop: 60
+	box: {
+		width: 150,
+		height: 150,
+		backgroundColor: "red"
 	}
 });
+
 
 export default App;
